37b01b54dc2299352c348225ae8ff7a14ded37b9
Ian Lozinski <iml@uber.com>
2016-11-04 15:08:02 -0700

    Fix imports

--- conn.go
+++ conn.go
@@ -21,9 +21,9 @@ import (
 
 	"golang.org/x/net/context"
 
-	"github.com/gocql/gocql/internal/lru"
+	"github.com/i/gocql/internal/lru"
 
-	"github.com/gocql/gocql/internal/streams"
+	"github.com/i/gocql/internal/streams"
 )
 
 var (
